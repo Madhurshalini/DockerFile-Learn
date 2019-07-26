be509eee5b53443ff16cefab4d90fb65a39fdc1f
Madhurshalini <38996417+Madhurshalini@users.noreply.github.com>
2019-07-26 14:20:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
+def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
 
-if __name == '__main__':
-    app.run(debug=True,port=5009, host='0.0.0.0')
+if __name__ == '__main__':
+    app.run(debug=True,port=5000, host='0.0.0.0')
 
